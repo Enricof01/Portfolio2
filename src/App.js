8a0bb4f596ae8983f1998c1a70a5b829a9104476
Enrico <enrico.fritz@student.reutlingen-university.de>
2026-03-24 18:46:30 +0100

    c

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 import MeltingPoint from "./components/Meltingpoint";
 import CalendarEmbed from "./components/CalenderEmbed"
 import VocabTrainerEmbed from "./components/VocabTrainerEmbed";
-import { Analytics } from "@vercel/analytics/react"
+
 import {
   BrowserRouter as Router,
   Route,
@@ -35,7 +35,7 @@ function App() {
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Analytics></Analytics>
+        
         <Navbar />
         <ScrollToTop />
         <Routes>
